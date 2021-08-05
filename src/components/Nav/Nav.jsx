88f89a717178ef26c9d8e1f423493a30b6cf5eb3
KolyaLisovskiy <lisovskii.kolya@gmail.com>
2021-08-05 16:59:18 +0300

    NavLink for prevent refresh page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,16 +1,16 @@
 import n from './Nav.module.scss';
 import UserName from './UserName/UserName';
-
+import { NavLink } from 'react-router-dom';
 const Nav = () => {
-    return(  
-        <nav className={n.nav}>
-          <UserName></UserName>
-        <a href="/profile">Профиль</a>
-        <a href="/dialogs">Сообщения</a>
-        <a href="/news">Новости</a>
-        <a href="/music">Музыка</a>
-        <a href="/settings" className={n.nav_last}>Настройки</a>
-      </nav>
-       );
-    }
-    export default Nav;
+  return (
+    <nav className={n.nav}>
+      <UserName></UserName>
+      <NavLink to="/profile" activeClassName={n.active}>Профиль</NavLink>
+      <NavLink to="/dialogs"activeClassName={n.active} >Сообщения</NavLink>
+      <NavLink to="/news"activeClassName={n.active} >Новости</NavLink>
+      <NavLink to="/music"activeClassName={n.active} >Музыка</NavLink>
+      <NavLink to="/settings" activeClassName={n.active} className={n.nav_last}>Настройки</NavLink>
+    </nav>
+  );
+}
+export default Nav;
